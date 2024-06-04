3ffbfe5de652a3682a873c358e4852338137e2c4
michee <nangmamichee0@gmail.com>
2024-06-04 21:12:42 +0000

    hotelApi 1.1.0

--- pkg/controller/bookingsController.go
+++ pkg/controller/bookingsController.go
@@ -5,9 +5,12 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/michee/pkg/models"
+	"github.com/michee/pkg/utils"
 	"github.com/stripe/stripe-go/v78"
 	"github.com/stripe/stripe-go/v78/checkout/session"
 )
@@ -28,6 +31,7 @@ func generatePaymentID() (string, error) {
 	return hex.EncodeToString(b), nil
 }
 
+
 func CreateBooking(w http.ResponseWriter, r *http.Request) {
 	var booking models.Booking
 	err := json.NewDecoder(r.Body).Decode(&booking)
@@ -36,6 +40,37 @@ func CreateBooking(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	startDate, err := time.Parse("2006-01-02", booking.StartDate)
+	if err != nil {
+		http.Error(w, "Invalid start date format", http.StatusBadRequest)
+		return
+	}
+	endDate, err := time.Parse("2006-01-02", booking.EndDate)
+	if err != nil {
+		http.Error(w, "Invalid end date format", http.StatusBadRequest)
+		return
+	}
+
+	duration := endDate.Sub(startDate)
+	numDays := int(duration.Hours() / 24) 
+
+	roomId := booking.RoomId
+
+	room, _ := models.GetRoomById(roomId)
+	
+	roomPrice := room.RoomPrice
+
+
+	roomPriceInt, err := strconv.Atoi(roomPrice)
+	if err != nil {
+			http.Error(w, "Failed to convert room price to integer", http.StatusInternalServerError)
+			return
+	}
+	
+	totalPrice := roomPriceInt * numDays
+	
+	booking.TotalPrice = strconv.Itoa(totalPrice)
+
 	paymentID, err := generatePaymentID()
 	if err != nil {
 		http.Error(w, "Failed to generate payment ID", http.StatusInternalServerError)
@@ -43,6 +78,8 @@ func CreateBooking(w http.ResponseWriter, r *http.Request) {
 	}
 
 	booking.PaymentId = paymentID
+	booking.PaymentStatus = true
+
 	booking.CreateBooking()
 
 	params := &stripe.CheckoutSessionParams{
@@ -54,12 +91,12 @@ func CreateBooking(w http.ResponseWriter, r *http.Request) {
 					ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
 						Name: stripe.String("Booking for " + booking.UserName),
 					},
-					UnitAmount: stripe.Int64(1000), 
+					UnitAmount: stripe.Int64(int64(totalPrice)), 
 				},
 				Quantity: stripe.Int64(1),
 			},
 		},
-		Mode: stripe.String("payment"),
+		Mode:       stripe.String("payment"),
 		SuccessURL: stripe.String("http://localhost:5000/booking/success?session_id={CHECKOUT_SESSION_ID}"),
 		CancelURL:  stripe.String("http://localhost:5000/booking/cancel"),
 	}
@@ -74,10 +111,8 @@ func CreateBooking(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
-
-	// http.Redirect(w, r, s.URL, http.StatusSeeOther)
-
 }
+
 
 func GetBooking(w http.ResponseWriter, r *http.Request) {
 	bookings := models.GetAllBooking()
@@ -87,7 +122,7 @@ func GetBooking(w http.ResponseWriter, r *http.Request) {
 
 func GetBookingById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	bookingId := params["id"]
+	bookingId := params["bookingId"]
 	booking, _ := models.GetBookingById(bookingId)
 	res, _ := json.Marshal(booking)
 	w.Header().Set("content-type", "application/json")
@@ -96,14 +131,40 @@ func GetBookingById(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateBooking(w http.ResponseWriter, r *http.Request) {
-	// Add your implementation here
+	bookingUpdate := models.Booking{}
+	utils.ParseBody(r, &bookingUpdate)
+	booking := mux.Vars(r)
+	bookingId := booking["bookingId"]
+	bookingDetail, db := models.GetBookingById(bookingId)
+
+	if bookingUpdate.Currency != ""{
+		bookingDetail.Currency = bookingUpdate.Currency
+	}
+	if bookingUpdate.StartDate != ""{
+		bookingDetail.StartDate = bookingUpdate.StartDate
+	}
+	if bookingUpdate.EndDate != ""{
+		bookingDetail.EndDate = bookingUpdate.EndDate
+	}
+	if bookingUpdate.UserName != ""{
+		bookingDetail.UserName = bookingUpdate.UserName
+	}
+	if bookingUpdate.UserEmail != ""{
+		bookingDetail.UserEmail = bookingUpdate.UserEmail
+	}
+
+	db.Save(&bookingDetail)
+	res, _ := json.Marshal(bookingDetail)
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func DeleteBooking(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	bookingId := params["id"]
-	models.DeleteById(bookingId)
-	res, _ := json.Marshal(bookingId)
+	books := models.DeleteById(bookingId)
+	res, _ := json.Marshal(books)
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
